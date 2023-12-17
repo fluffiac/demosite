dcf1d004bd1ce5706f5485ec5c147ad56dda8224
fluffiac <fluffiac@protonmail.com>
2023-12-17 14:25:09 -0800

    debug message logging

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,36 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+async function ws(addr: string): Promise<WebSocket> {
+    let ws = new WebSocket(addr);
+
+    return new Promise((res, rej) => {
+        ws.onopen = () => res(ws)
+        ws.onerror = () => rej()
+    })
+}
+
+async function repeat<T>(p: () => Promise<T>): Promise<T> {
+    return p().catch(_ => repeat(p))
+}
+
+
+
+export class SparrowSocket {
+    ws = repeat(() => ws("ws://127.0.0.1:8666")).then((ws) => this.sockinit(ws))
+
+    onopen = () => {}
+    onclose = () => {}
+    onmesg = (mesg: string) => {}
+
+    sockinit(sock: WebSocket) {
+        this.onopen();
+
+        const reinit = () => {
+            this.onclose();
+            this.ws = repeat(() => ws("ws://127.0.0.1:8666")).then((ws) => this.sockinit(ws))
+        }
+
+        sock.onclose = reinit;
+        sock.onmessage = (e) => this.onmesg(e.data);
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
+    import { SparrowSocket } from "$lib";
     import { onMount } from "svelte";
+    
 
     let mesgs: string[] = [];
 
-    onMount(() => {
-        let ws = new WebSocket("ws://127.0.0.1:8080");
+    function push(mesg: string) {
+        console.log(mesg);
+        mesgs.push(mesg);
+        mesgs = mesgs;
+    }
 
-        ws.onmessage = (e) => {
-            console.log(e.data);
-        }
+    onMount(() => {
+        let sock = new SparrowSocket();
+
+        sock.onmesg = (mesg) => push(mesg)
+        sock.onopen = () => push('SocketOpen')
+        sock.onclose = () => push('SocketClose')
     })
 </script>
 
-<h1>gh deploy test</h1>
+<button on:click={() => mesgs = []}>Clear</button>
+{#each mesgs as mesg}
+    <p>{mesg}</p>
+{/each}
+
+<style>
+    p {
+        margin: 0;
+        white-space: nowrap
+    }
+</style>
