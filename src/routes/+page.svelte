54f21ed7a7b69a814901a6e51186b43dd9a345b2
fluffiac <fluffiac@protonmail.com>
2023-12-17 12:46:36 -0800

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let mesgs: string[] = [];
+
+    onMount(() => {
+        let ws = new WebSocket("ws://127.0.0.1:8080");
+
+        ws.onmessage = (e) => {
+            console.log(e.data);
+        }
+    })
+</script>
+
+<h1>gh deploy test</h1>
